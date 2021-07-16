83d5f870e1ba75fc3b4facf159fe02fa47d06f35
Janduy <Janduy@CALOPSITA>
2021-07-16 20:44:24 -0300

    Add project files.

---
+++ ProjetoDIOMVC/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoDIOMVC.Models
+{
+    public class Context: DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;DataBase=ProjetoDIOMVC;Integrated Security=True");
+        }
+    }
+}
---
+++ ProjetoDIOMVC/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoDIOMVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
